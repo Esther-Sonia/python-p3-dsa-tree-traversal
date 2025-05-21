93cd1df8eda338f3f9a8ee1761137c92e4fce278
Esther-Sonia <esthersonia21@gmail.com>
2025-05-21 15:37:25 +0300

    implement get_element_by_id using depth-first traversal

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,18 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    return self._dfs(self.root, id)
+  
+  def _dfs(self, node, id):
+    if node is None:
+      return None
+    
+    if node['id'] == id:
+      return node
+    
+    for child in node['children']:
+      result = self._dfs(child, id)
+      if result is not None:
+        return result
+      
+    return None  
